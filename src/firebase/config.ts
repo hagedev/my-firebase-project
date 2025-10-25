8bcfc3d5a9aae3e9cff3a2dbd738d088aeeba73f
Hari Putra Bayu <hariputrabayu@gmail.com>
2025-10-25 07:02:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1358901660-8dbfc",
+  "appId": "1:930192903723:web:b648e5e028425fd4a8a46e",
+  "apiKey": "AIzaSyDytziNFkJyP1oTNIEBH4TA1nxIC1FR2-Y",
+  "authDomain": "studio-1358901660-8dbfc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "930192903723"
+};
