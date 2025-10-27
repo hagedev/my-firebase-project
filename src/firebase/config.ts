5eda651eb2633c180748bb3b415b86ef2b323444
Hari Putra Bayu <hariputrabayu@gmail.com>
2025-10-27 08:59:02 +0000

    Web apps Firebase app Web App App nickname Firebase app App ID 1:447992

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   "projectId": "studio-1732414273-21518",
-  "appId": "1:447992347393:web:b8d80c65c2787d1326c2f7",
+  "appId": "1:447992347393:web:eefb7f78cdbbc41e74b900",
   "apiKey": "AIzaSyAi8CB7z1c0cT5LNRNaZijUd4xgDg_8l_U",
   "authDomain": "studio-1732414273-21518.firebaseapp.com",
   "storageBucket": "studio-1732414273-21518.appspot.com",
